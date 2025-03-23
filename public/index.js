e5144ccdeb79558254d3b7e9cded332484ce5cf9
ikedam <devld@ikedam.jp>
2025-03-23 13:41:06 +0900

    Improve the page

--- public/index.js
+++ public/index.js
@@ -5,14 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(response => {
             if (response.ok) {
-                alert('WOL command sent successfully!');
+                alert('起動したと思う');
             } else {
-                alert('Failed to send WOL command.');
+                console.error('failed to launch: %o', response)
+                alert('起動失敗');
             }
         })
         .catch(error => {
-            console.error('Error:', error);
-            alert('An error occurred.');
+            console.error('Error: %o', error);
+            alert('起動失敗');
         });
     });
 });
