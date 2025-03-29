6a934b111dfeaa8bed9a9e4af4537fe285f801f2
ikedam <devld@ikedam.jp>
2025-03-30 00:56:18 +0900

    wolnut はダウンロードして使う方式とした。

---
+++ wolnut/wolnut.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+: ${VERSION:=v1.0.2}
+: ${WORKDIR:=/tmp/wolnut}
+: ${REPEAT:=10}
+: ${SLEEP:=10}
+
+if [ ! -d "${WORKDIR}" ]; then
+  mkdir -p "${WORKDIR}"
+fi
+
+for i in $(seq "${REPEAT}"); do
+  wget -q -O "${WORKDIR}/wolnut" "https://github.com/ikedam/wollet/releases/download/${VERSION}/wolnut-linux-mipsle-softfloat" && true
+  if [ "${?}" = "0" ]; then
+    break
+  fi
+  if [ "${i}" == "${REPEAT}" ]; then
+    echo "give up!" >&2
+    exit 1
+  fi
+  echo "fail(${i})...Retry after ${SLEEP} secs" >&2
+  sleep "${SLEEP}"
+done
+
+chmod 755 "${WORKDIR}/wolnut"
+cp "$(dirname "$0")/wolnut.yaml" "${WORKDIR}"
+
+exec "${WORKDIR}/wolnut" "$@"
